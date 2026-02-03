4727c39f079b7c8fc9d39abd218d52a2bab9c117
CharlesC03 <ceciampa@gmail.com>
2026-02-03 22:37:32 +0000

    Deploying to gh-pages from @ CharlesC03/CharlesC03.github.io@cd7ceb2f3abc5b954aaaaee1ae3ee7ba0c0a0ff1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,36 +28,36 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_1/";
-            },},{id: "projects-high-speed-footage-analyzer",
-          title: 'High Speed Footage Analyzer',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/Footage_Analyzer/";
-            },},{id: "projects-estimating-trajectory-from-imu",
-          title: 'Estimating Trajectory From IMU',
-          description: "Estimated vehicle trajectory exclusively from IMU by fusing magnetometer and gyroscope data, validated against GPS",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/IMU/";
-            },},{id: "projects-improved-managebac",
-          title: 'Improved Managebac',
-          description: "Made an extension to improve Managebac.",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/Improved_Managebac/";
-            },},{id: "projects-automatic-lecture-reducer",
-          title: 'Automatic Lecture Reducer',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/Lecture_Reducer/";
-            },},{id: "projects-estimated-transit-eta",
-          title: 'Estimated Transit ETA',
-          description: "Using data provided by the MBTA estimated my trip to school estimated length and expected time using Monte-Carlo.",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/MBTA_ETA/";
             },},{id: "projects-chess-bot-player-move-predictor",
           title: 'Chess Bot Player Move Predictor',
           description: "A deep learning AI model which can predict a players move from the previous moves they made.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/chess_ai/";
+            },},{id: "projects-high-speed-footage-analyzer",
+          title: 'High Speed Footage Analyzer',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/footage_analyzer/";
+            },},{id: "projects-improved-managebac",
+          title: 'Improved Managebac',
+          description: "Made an extension to improve Managebac.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/improved_managebac/";
+            },},{id: "projects-estimating-trajectory-from-imu",
+          title: 'Estimating Trajectory From IMU',
+          description: "Estimated vehicle trajectory exclusively from IMU by fusing magnetometer and gyroscope data, validated against GPS",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/imu/";
+            },},{id: "projects-automatic-lecture-reducer",
+          title: 'Automatic Lecture Reducer',
+          description: "a project with no image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/lecture_reducer/";
+            },},{id: "projects-estimated-transit-eta",
+          title: 'Estimated Transit ETA',
+          description: "Using data provided by the MBTA estimated my trip to school estimated length and expected time using Monte-Carlo.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/mbta_eta/";
             },},{id: "projects-bias-transfer-in-model-distillation",
           title: 'Bias Transfer in Model Distillation',
           description: "Investigated bias propagation during knowledge distillation from Llama 3.1 8B Instruct to smaller student architectures (BoW, CNN, DistilBERT), quantifying measurable increases in racial and gender bias across student models.",
